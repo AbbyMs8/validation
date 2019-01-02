b3a08c07ccfd3f0235a6edd69fd81ddc1019722a
zhao.lin <zhao.lin@mgm-iot.com>
2019-01-02 19:28:37 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "1",
+            "tab"
+        ],
+        "linebreak-style": [
+            "0",
+            "windows"
+        ],
+        "quotes": [
+            "1",
+            "double"
+        ],
+        "semi": [
+            "2",
+            "always"
+        ]
+    }
+};
